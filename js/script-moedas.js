6d75f8529fa5f5dceb5116168cc5821b0ef05771
vitoria <vitoria.evangelista353@gmail.com>
2022-11-11 22:33:02 -0300

    Continuação script-moedas.js

--- js/script-moedas.js
+++ js/script-moedas.js
@@ -24,3 +24,50 @@ class MobileNavbar {
       this.mobileMenu.classList.toggle(this.activeClass);
       this.animateLinks();
     }
+    
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+
+
+
+
+
+
+
+/* SCRIPT CONVERSOR MOEDAS */
+
+
+
+
+let valorDigitado = document.querySelector("#valorReal");
+
+
+let moedaSelecionada = document.getElementsByName("moedaEstrangeira");
+
+let aviso = document.querySelector("#aviso");
+
+// selecionar os botoes
+let btnConverter = document.querySelector("#btnConverter");
+let btnLimpar = document.querySelector("#btnLimpar");
+
+let valorDolar = 5.18;      // dia 21/09/2022
+let valorEuro = 5.12;     // dia 21/09/2022
+let valorLibra = 5.87;     // dia 21/09/2022
+let valorYuan = 1.36;     // dia 21/09/2022
+let valorReal = 0;
