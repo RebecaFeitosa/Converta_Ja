7ba4dcd83ae2352c08fa26b050dbfac5ec0e95bc
vitoria <vitoria.evangelista353@gmail.com>
2022-11-15 15:27:00 -0300

    Ultima parte script-horas.js

--- js/script-horas.js
+++ js/script-horas.js
@@ -89,3 +89,46 @@ function ativarBotao() {
     console.log("Não ativou")
   }
 }
+
+
+btnConverter.addEventListener('click', function () {
+
+    valorEmTempo = parseFloat(valorDigitado.value)
+  
+    console.log('Escolha a conversao')
+    for (let i = 0; i < Horas_Minutos.length; i++) {
+      if (Horas_Minutos[i].checked) {
+        TempoSelecionado = Horas_Minutos[i].value
+        console.log(TempoSelecionado) //
+      }
+    }
+  
+    switch (TempoSelecionado) {
+  
+      case "Horas":
+        Converte = valorEmTempo * 60
+        mensagemFormatada(parseFloat(Converte.toFixed(2)) + " Minutos ")
+        break
+  
+      case "Minutos":
+        Converte = valorEmTempo / 60
+        mensagemFormatada(parseFloat(Converte.toFixed(2)) + " Horas ")
+        break
+  
+  
+      default:
+        aviso.textContent = "Escolha uma conversão!"
+  
+        isNaN(Converte) ? Converte = 0 : ""
+    }
+  })
+  
+  btnLimpar.addEventListener("click", function () {
+    valorDigitado.focus()
+    valorDigitado.value = ""
+    aviso.textContent = "Digite um número, escolha a conversão e ConvertaJá"
+    Horas_Minutos[0].checked = false
+    Horas_Minutos[1].checked = false //
+  
+  })
+  
